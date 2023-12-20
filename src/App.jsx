9bf9e83b0d9c215721833f5ce17410e8bb5a65fc
Byte Astro <biswajitswain348@gmail.com>
2023-12-20 18:34:41 +0530

    szc2da

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoWrapper } from './components/TodoWrapper'
 
 function App() {
-
   return (
     <>
-      <h1>Lol</h1>
+      <TodoWrapper />
     </>
   )
 }
